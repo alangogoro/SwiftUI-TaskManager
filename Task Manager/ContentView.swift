8f41da46bcddf15c0eb5f464764290f3147792ad
alangogoro <cayne9160@gmail.com>
2024-11-01 01:19:41 +0800

    Initial Commit

---
+++ Task Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task Manager
+//
+//  Created by usr on 2024/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
